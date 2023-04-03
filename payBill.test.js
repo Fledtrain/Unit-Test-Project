3253d4c1e7fe2f994fa8c3023fcc9304d79c0578
Fledtrain <Joeyew2014@gmail.com>
2023-04-02 21:58:24 -0400

    Fixed some changes

--- payBill.test.js
+++ payBill.test.js
@@ -1,4 +1,4 @@
-const payBill = require("./payBill");
+const payBill = require("./payBill"); //Import the payBill function 
 
 const totalBill = parseFloat(process.argv[2]);
 const numGuests = parseInt(process.argv[3]);
@@ -9,7 +9,6 @@ if (isNaN(totalBill) || isNaN(numGuests)) {
   );
 } else {
   const guestAmounts = payBill(totalBill, numGuests);
-
   console.log("The following amounts are owed by each guest:");
   guestAmounts.forEach((amount, index) => {
     console.log(`Guest ${index + 1}: $${amount}`);
