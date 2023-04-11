35ac9215104553c45dfd93f53981af8ec65c842c
Fledtrain <Joeyew2014@gmail.com>
2023-04-11 11:48:13 -0400

    Added some comments

--- payBill.test.js
+++ payBill.test.js
@@ -1,13 +1,16 @@
 const payBill = require("./payBill"); //Import the payBill function 
 
+//Get the total bill amount and number of guests from the command line
 const totalBill = parseFloat(process.argv[2]);
 const numGuests = parseInt(process.argv[3]);
 
+// Check if the total bill amount and number of guests are valid
 if (isNaN(totalBill) || isNaN(numGuests)) {
   console.log(
     "Error: Please provide a valid total bill amount and number of guests."
   );
 } else {
+  // Call the payBill function and print the guest amounts
   const guestAmounts = payBill(totalBill, numGuests);
   console.log("The following amounts are owed by each guest:");
   guestAmounts.forEach((amount, index) => {
