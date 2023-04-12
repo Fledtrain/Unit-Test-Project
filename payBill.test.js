2e1822a1bccd1ea55388c65e7893d735cda3b7e5
Fledtrain <Joeyew2014@gmail.com>
2023-04-11 22:29:28 -0400

    New version using Jest

--- payBill.test.js
+++ payBill.test.js
@@ -1,19 +1,36 @@
-const payBill = require("./payBill"); //Import the payBill function 
+const payBill = require("./payBill");
 
-//Get the total bill amount and number of guests from the command line
-const totalBill = parseFloat(process.argv[2]);
-const numGuests = parseInt(process.argv[3]);
-
-// Check if the total bill amount and number of guests are valid
-if (isNaN(totalBill) || isNaN(numGuests)) {
-  console.log(
-    "Error: Please provide a valid total bill amount and number of guests."
-  );
-} else {
-  // Call the payBill function and print the guest amounts
+// Test case 1: Valid input values
+test("Valid input values", () => {
+  const totalBill = 15.16;
+  const numGuests = 3;
   const guestAmounts = payBill(totalBill, numGuests);
-  console.log("The following amounts are owed by each guest:");
-  guestAmounts.forEach((amount, index) => {
-    console.log(`Guest ${index + 1}: $${amount}`);
+
+  // Ensure the total amount is distributed correctly among the guests
+  expect(guestAmounts.length).toBe(numGuests);
+  expect(guestAmounts.reduce((sum, amount) => sum + amount, 0)).toBe(totalBill);
+});
+
+// Test case 2: Test with uneven amounts
+test("Test with uneven amounts", () => {
+  const totalBill = 25.32;
+  const numGuests = 4;
+  const guestAmounts = payBill(totalBill, numGuests);
+
+  // Ensure the total amount is distributed correctly among the guests
+  expect(guestAmounts.length).toBe(numGuests);
+  expect(guestAmounts.reduce((sum, amount) => sum + amount, 0)).toBe(totalBill);
+
+  // Ensure the amounts are rounded to 2 decimal places
+  guestAmounts.forEach((amount) => {
+    expect(amount).toBeCloseTo(amount, 2);
   });
-}
+});
+// Test case 3: Test with NaN input for number of guests and bill amount
+test("Test with NaN input for number of guests and bill amount", () => {
+  const totalBill = NaN;
+  const numGuests = NaN;
+
+  // Expect an error to be thrown when calling payBill with NaN input values
+  expect(() => payBill(totalBill, numGuests)).toThrow();
+});
